9cc7badbc0637fbf81c71f26c2c9e3515981a73f
yusuke2513 <yusukekamata@keio.jp>
2026-02-11 22:25:42 +0900

    Vercel用の変更

--- database/products.js
+++ database/products.js
@@ -3,6 +3,7 @@
 
 export async function fetchProducts() {
   // const res = await fetch('http://localhost:3001/api/products');
-  const res = await fetch('http://192.168.1.7:3001/api/products');
+  // const res = await fetch('http://192.168.1.7:3001/api/products');
+  const res = await fetch('https://donut-application.onrender.com/api/products');
   return await res.json();
 }
--- database/toppings.js
+++ database/toppings.js
@@ -3,9 +3,11 @@
 export const fetchToppings = async () => {
   try {
     // スマホアクセスに対応するため、現在のホスト名を自動取得
-    const host = window.location.hostname;
+    // const host = window.location.hostname;
     // バックエンドサーバーのポート3001へ接続
-    const response = await fetch(`http://${host}:3001/api/toppings`);
+    // const response = await fetch(`http://${host}:3001/api/toppings`);
+    const API_BASE_URL = "https://donut-application.onrender.com";
+    const response = await fetch(`${API_BASE_URL}/api/toppings`);
     
     if (!response.ok) {
       throw new Error("トッピングデータの取得に失敗しました");
